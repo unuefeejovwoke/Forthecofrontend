eaaa73d6dd3cced6157447c2c6f626a91a9adb4c
lumberjack001 <itoya.shem2017@gmail.com>
2023-03-04 19:34:20 +0100

    fixed responsive issues

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,32 @@
+//registeration page
+const loginBtn = document.querySelector('#loginBtn');
+const signUpBtn = document.querySelector('#signUpBtn');
+const loginPage = document.querySelector('#Login');
+const signUpPage = document.querySelector('#Register');
+
+setTimeout( ()=>{
+    loginBtn.addEventListener('click', ()=>{
+        loginPage.classList.add('d-show')
+        signUpPage.classList.remove('d-show')
+        signUpPage.classList.add('d-none')
+        loginBtn.classList.add('active')
+        signUpBtn.classList.remove('active')
+    })
+}, 2000)
+
+ setTimeout(()=>{
+    signUpBtn.addEventListener('click', ()=>{
+        signUpPage.classList.remove('d-none')
+        signUpPage.classList.add('d-show')
+        loginPage.classList.remove('d-show')
+        loginPage.classList.add('d-none')
+        loginBtn.classList.remove('active')
+        signUpBtn.classList.add('active')
+    
+    })
+ }, 2000)
+
+
 "use strict";
 
 const heading4 = document.querySelector(".abbreviation");
@@ -7,5 +36,5 @@ const headWidth = heading4.offsetWidth;
 const headLeft = heading4.offsetLeft;
 
 customUnderline.style.width = headWidth + "px";
-// customUnderline.style.left = headLeft + "px";
+customUnderline.style.left = headLeft + "px";
 
