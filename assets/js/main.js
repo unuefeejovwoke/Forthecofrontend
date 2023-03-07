2568a6aceed77b6f8136242108b747a3b1237487
Mateen.io <gbadamosimateen@gmail.com>
2023-03-07 06:23:18 +0100

    wrote script for toggling password reveal

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,30 @@ const signUpBtn = document.querySelector("#signUpBtn");
 const loginPage = document.querySelector("#Login");
 const signUpPage = document.querySelector("#Register");
 
+const pwrd = document.querySelectorAll("#pwrdHASHED");
+const reveal = document.querySelectorAll(".ri-eye-line");
+
+const pwrdReveal = function () {
+  for (let j = 0; j < pwrd.length; j++) {
+    if (pwrd[j].type === "password") {
+      pwrd[j].type = "text";
+
+      reveal[j].classList.add("ri-eye-off-line");
+      reveal[j].classList.remove("ri-eye-line");
+    } else {
+      pwrd[j].type = "password";
+      reveal[j].classList.add("ri-eye-line");
+      reveal[j].classList.remove("ri-eye-off-line");
+    }
+  }
+};
+
+for (let i = 0; i < reveal.length; i++) {
+  reveal[i].addEventListener("click", pwrdReveal);
+}
+
+// reveal.addEventListener("click", pwrdReveal);
+
 setTimeout(() => {
   loginBtn.addEventListener("click", () => {
     loginPage.classList.add("d-show");
@@ -50,9 +74,9 @@ const openModal = function () {
 };
 
 const openProblemModal = function () {
-    problemModal.classList.remove("hidden");
-    overlay.classList.remove("hidden");
-  };
+  problemModal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
 
 const closeModal = function () {
   projectModal.classList.add("hidden");
