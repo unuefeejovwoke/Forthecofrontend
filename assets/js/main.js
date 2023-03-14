ec2ffcab8cd0421f1fd135f9e5808a32cba78eb2
lumberjack001 <itoya.shem2017@gmail.com>
2023-03-14 13:37:52 +0100

    fixes dropdown

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,26 +8,27 @@ const navbarReveal = function(){
 
 navToggle.addEventListener('click', navbarReveal);
 //nav-dropdown
-const dropBtn = document.querySelector(".dropbtn")
-const dropContent = document.querySelector(".dropdown-content")
+// const dropBtn = document.querySelector(".dropbtn")
+// const dropContent = document.querySelector(".dropdown-content")
+// const navBar = document.querySelector("nav");
 
-dropBtn.addEventListener("click", (e)=>{
-  if(e.target === dropBtn){
-    dropContent.classList.remove("d-none");
-  dropContent.classList.add("d-show")
-  }else{
-    dropContent.add("d-none")
-  }
+// dropBtn.addEventListener("click", (e)=>{
+//   if(e.target === dropBtn){
+//     dropContent.classList.remove("d-none");
+//   dropContent.classList.add("d-show")
+//   }else{
+//     dropContent.add("d-none")
+//   }
 
-  console.log("clicked dropdown")
-});
-window.onclick = function(e){
-  if (!e.target.matches('.dropbtn') && 
-        dropContent.classList.contains('d-show')){
-      dropContent.classList.remove('d-show');
-      dropContent.classList.add("d-none")
- }
-}
+//   console.log("clicked dropdown")
+// });
+// window.addEventListener('click', (e)=>{
+//   console.log(e.target)
+//   if (!e.target.matches('dropBtn')){
+//       dropContent.classList.remove('d-show');
+//       dropContent.classList.add("d-none")
+//  }
+// })
 
 ("use strict");
 
